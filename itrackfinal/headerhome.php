28120ae6fb41e08055d69a98c26febdda2349f6d
deftrish <47418300+deftrish@users.noreply.github.com>
2019-02-07 19:45:39 +0800

    Add files via upload

---
+++ itrackfinal/headerhome.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.min.css">
+<style>
+<?php include 'assets/css/style.css'; ?>
+</style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark itrackbar">
+  <a class="navbar-brand" href="#">iTrack <br> <h6> Community Relationship Management and Feedback System </h6></a>
+    
+</nav>
---
+++ itrackfinal/index.php
@@ -0,0 +1,73 @@
+<?php
+	require "headerhome.php";
+?>
+
+		<main>
+
+<head>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+span.psw {
+  float: left;
+  padding-top:10px;
+}
+
+/* Change styles for span and cancel button on extra small screens */
+@media screen and (max-width: 500px) {
+  span.psw {
+     display: block;
+     float: none;
+  }
+
+
+</style>
+</head>
+<body>
+
+
+
+<div class="container">
+        <div class="row justify-content-center align-items-center" style="height:60vh" >
+            <div class="col-4">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="home_itrack.php" autocomplete="off">
+                        	<h4>iTrack Login</h4>
+							<div class="form-group">
+                                <b>Username</b> <input type="text" class="form-control" name="username">
+                            </div>
+                            <div class="form-group">
+                                <b>Password</b><input type="password" class="form-control" name="password">
+                            </div>
+                            <button type="button" id="sendlogin" class="btn btn-primary">Sign In</button>
+                        </form>
+                        <span class="psw"><a href="#">Forgot password?</a></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+
+
+      
+
+  </div>
+
+
+  
+  </div>
+</form>
+
+</body>
+
+		</main>
+
+<?php
+	require "footer.php";
+?>
